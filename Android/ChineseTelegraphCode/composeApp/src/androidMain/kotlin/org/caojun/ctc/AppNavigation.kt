bf75dedd82f73a3dba99782c30b45e4665870bcc
JadeCao <jadecao@tristarinc.com>
2025-07-10 20:34:57 +0800

    优化代码

--- Android/ChineseTelegraphCode/composeApp/src/androidMain/kotlin/org/caojun/ctc/AppNavigation.kt
+++ Android/ChineseTelegraphCode/composeApp/src/androidMain/kotlin/org/caojun/ctc/AppNavigation.kt
@@ -14,7 +14,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -22,7 +22,7 @@ import androidx.compose.ui.Modifier
 
 @Composable
 fun AppNavigation() {
-    var selectedTab by remember { mutableStateOf(0) }
+    var selectedTab by remember { mutableIntStateOf(0) }
 
     Scaffold(
         bottomBar = {
