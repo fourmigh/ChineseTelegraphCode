47c794f5da91a6f7022cfa888dd6e214bfcb5f32
JadeCao <jadecao@tristarinc.com>
2025-07-17 21:34:34 +0800

    优化交互

--- Android/ChineseTelegraphCode/composeApp/src/androidMain/kotlin/org/caojun/ctc/main/CTCScreen.kt
+++ Android/ChineseTelegraphCode/composeApp/src/androidMain/kotlin/org/caojun/ctc/main/CTCScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -23,6 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -82,6 +84,7 @@ fun CTCScreen() {
                 value = tfvKey,
                 onValueChange = { tfvKey = it },
                 label = { Text("密钥") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                 modifier = Modifier.fillMaxWidth()
             )
         }
